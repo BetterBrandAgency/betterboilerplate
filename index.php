0eeb6b08b35b13d4cbc9b82d324840d28b6d28da
Steven Roberts <ste.matchboxhero@gmail.com>
2017-08-21 10:37:06 +0100

    new grid system and new mixins added, started on widgets

--- index.php
+++ index.php
@@ -189,6 +189,11 @@
 
     </div>
 
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"
+        integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+        crossorigin="anonymous">
+    </script>
+
     <script src="dist/js/main.min.js"></script>
 
 </body>
