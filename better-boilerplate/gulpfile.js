00c20eac98a5651526853921fcd9e1614fbcb072
Steven Roberts <ste.matchboxhero@gmail.com>
2018-03-05 12:35:38 +0000

    Update gulpfile.js

--- better-boilerplate/gulpfile.js
+++ better-boilerplate/gulpfile.js
@@ -431,7 +431,7 @@
             gulp.watch('src/images/**/*', ['images', 'svg-images']);
 
             // Watch SVGs
-            gulp.watch('src/svg/**/*', ['svg-sprite']);
+            gulp.watch('src/svgs/**/*', ['svg-sprite']);
 
         });
 
