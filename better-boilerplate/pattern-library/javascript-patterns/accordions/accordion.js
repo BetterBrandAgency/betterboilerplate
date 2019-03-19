c3ee9caabbe5ebb3f93fae5a554d182b9860b43e
Steven Roberts <ste.matchboxhero@gmail.com>
2019-03-19 11:19:53 +0000

    Updated Accordion JS (a11y)
    Added JS to change the 'aria-expanded' value when clicked

--- better-boilerplate/pattern-library/javascript-patterns/accordions/accordion.js
+++ better-boilerplate/pattern-library/javascript-patterns/accordions/accordion.js
@@ -1,12 +1,21 @@
 // Accordions
-	var accordion__title = '.js-accordion__title';
-	var accordion__content = '.js-accordion__content';
+    var accordion__title = '.js-accordion__title';
+    var accordion__content = '.js-accordion__content';
 
-	$(accordion__content).slideUp();
-	$('.is-active').parent().find(accordion__content).slideDown();
+    $(accordion__content).slideUp();
+    $('.is-active').parent().find(accordion__content).slideDown();
 
-	$(accordion__title).click( function () {
-		$(this).toggleClass('is-active');
-		$(this).next(accordion__content).slideToggle();
-		return false;
-	});
+    $(accordion__title).click( function () {
+	$(this).toggleClass('is-active');
+	$(this).next(accordion__content).slideToggle();
+
+	console.log($(this).hasClass('is-active'));
+
+	if($(this).hasClass('is-active')) {
+	    $(this).attr('aria-expanded', 'true');
+	} else {
+	    $(this).attr('aria-expanded', 'false');
+	}
+
+	return false;
+    });
